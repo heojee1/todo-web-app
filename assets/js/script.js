63cac98592f864cba24f487ac892816c8d88f7e1
heojee1 <heojee1@gmail.com>
2020-09-23 20:35:54 +0900

    add add-todo functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 /* check off a todo item */
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('checked');
 });
 
 /* delete a todo item */
-$('span').on('click', function(event) {
+$('ul').on('click', 'span', function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation(); /* stop the event from triggering event listeners on parent elements */
-})
+});
+
+/* add a new todo item */
+$('input[type="text"]').on('keypress', function(event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val('');
+        $('ul').append('<li><span>x</span> ' + todoText + '</li>');
+    }
+});
